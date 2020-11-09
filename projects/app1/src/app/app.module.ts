94d106e2dd299be7d214e0ad25e6998ff1b0899b
christopher-harris <ccharris0607@outlook.com>
2020-11-09 09:31:42 -0600

    Adds new code to a project and adds a new module to the same project to test out app how it would look in production.

--- projects/app1/src/app/app.module.ts
+++ projects/app1/src/app/app.module.ts
@@ -1,11 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
+import {BrowserModule} from '@angular/platform-browser';
 import {ModuleWithProviders, NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { View1Component } from './view1/view1.component';
-import { View2Component } from './view2/view2.component';
-import { NavComponent } from './nav/nav.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {View1Component} from './view1/view1.component';
+import {View2Component} from './view2/view2.component';
+import {NavComponent} from './nav/nav.component';
+import {DashboardStatsComponent} from './dashboard-stats/dashboard-stats.component';
+import {MaterialModule} from '../../../../src/app/shared/material/material.module';
 
 const app1Providers = [];
 
@@ -14,16 +16,19 @@ const app1Providers = [];
     AppComponent,
     View1Component,
     View2Component,
-    NavComponent
+    NavComponent,
+    DashboardStatsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
 
 @NgModule({})
 export class App1SharedModule {
